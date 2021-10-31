8d0560ed89f9ce8a9e18dcebccf9fc5f7695e775
Epousek <epousek@seznam.cz>
2021-10-31 13:06:47 +0100

    added Censor tag

---
+++ Censor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Tags
+{
+  class Censor
+  {
+    public static void Apply(string text)
+    {
+      text = Regex.Replace(text, @"[a-zA-Z0-9]", "*");
+      Console.Write(text);
+    }
+  }
+}
