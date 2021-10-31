bb1b825f593948d35ce6a1e0898eb31fb943c7f8
Epousek <epousek@seznam.cz>
2021-10-31 13:01:10 +0100

    added capital tag

---
+++ Capital.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tags
+{
+  static class Capital
+  {
+    public static void Apply(string text)
+    {
+      text = text.ToUpper();
+      Console.Write(text);
+    }
+  }
+}
