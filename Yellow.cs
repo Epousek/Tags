1292c4298a298c26609901530f90516f9b9b6489
Epousek <epousek@seznam.cz>
2021-10-31 13:02:40 +0100

    added Yellow tag

---
+++ Yellow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tags
+{
+  class Yellow
+  {
+    public static void Apply(string text)
+    {
+      Console.ForegroundColor = ConsoleColor.DarkYellow;
+      Console.Write(text);
+      Console.ForegroundColor = ConsoleColor.White;
+    }
+  }
+}
